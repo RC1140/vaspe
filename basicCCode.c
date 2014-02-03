a09a7fc2a0c3cb3c9380afc02d71619b93a7f620
rc1140 <rc1140@republiccommandos.co.za>
2014-02-03 14:40:12 +0200

    more changes

--- basicCCode.c
+++ basicCCode.c
@@ -7,8 +7,12 @@ int main(int argc,char** argv)
     fp = fopen ("myfile.txt","w");
     if (fp!=NULL)
     {
-        fputs ("fopen example",fp);
-        fclose (fp);
+        puts("Some message about not being able to open the file");
     }
+    int c;
+    do {
+      c = fgetc (fp);
+      if (c == '$') n++;
+    } while (c != EOF);
     return 0;
 }
